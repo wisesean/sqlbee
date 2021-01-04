8ef14b2a154198f8c8083b0728673c8fa90b9fa8
wisesean <wisesean@foxmail.com>
2021-01-04 16:03:55 +0800

    bugfix(delete): 修复deletebug

--- builder/src/test/java/site/autzone/sqlbee/sql/delete/SqlDeleteBuilderTest.java
+++ builder/src/test/java/site/autzone/sqlbee/sql/delete/SqlDeleteBuilderTest.java
@@ -8,9 +8,13 @@ import site.autzone.sqlbee.builder.SqlBuilder;
 public class SqlDeleteBuilderTest {
     @Test
     public void testApp() throws Exception {
-        Assert.assertEquals("DELETE * FROM TABLE1", SqlBuilder.createDelete().table("TABLE1").build().output());
-        Assert.assertEquals("DELETE * FROM TABLE1 AS T1", SqlBuilder.createDelete().table("TABLE1", "T1").build().output());
-        Assert.assertEquals("DELETE T1.* FROM TABLE1 AS T1", SqlBuilder.createDelete().table("TABLE1", "T1").end().select().column("T1.*").end().build().output());
-        Assert.assertEquals("DELETE T1.* FROM TABLE1 AS T1 WHERE (T1.ID = ?)", SqlBuilder.createDelete().table("TABLE1", "T1").end().select().column("T1.*").end().condition("=").left("T1.ID").right(new Value(1111)).end().build().output());
+        Assert.assertEquals("DELETE FROM TABLE1", SqlBuilder.createDelete()
+                .table("TABLE1").sql().prepareSql());
+        Assert.assertEquals("DELETE FROM TABLE1 AS T1", SqlBuilder.createDelete()
+                .table("TABLE1", "T1").sql().prepareSql());
+        Assert.assertEquals("DELETE FROM TABLE1 AS T1 WHERE (T1.ID = ?)", SqlBuilder.createDelete()
+                .table("TABLE1", "T1")
+                .condition("=").left("T1.ID").right(new Value(1111)).end()
+                .sql().prepareSql());
     }
 }
